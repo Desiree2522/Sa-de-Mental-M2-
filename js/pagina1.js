5310b587b76c42687e6b67596947d3185cae5907
Brenda-Louizi <brendalouizimarinhodesouza@gmail.com>
2024-12-16 15:57:57 -0300

    colaboradores

--- js/pagina1.js
+++ js/pagina1.js
@@ -1,18 +1,46 @@
+// Efeito de entrada no começo
+const myObserver = new IntersectionObserver((entries) => {
+   entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+         entry.target.classList.add('show');
+      } else {
+         entry.target.classList.remove('show');
+      }
+   });
+});
 
+const elements = document.querySelectorAll('.hidden');
+elements.forEach((element) => myObserver.observe(element));
 
-const my0bserver = new IntersectionObserver ( (entries)  => {
-   entries.forEach ((entry) => {
-if(entry.isIntersecting){
-    entry.target.classList.add(`show`)
- } else  {
-   entry.target.classList.remove(`show`)
+// Swiper Configuração
+const swiper = new Swiper('.slider-wrapper', {
+   loop: true,
+   grabCursor: true,
+   spaceBetween: 30,
 
-   
-    }
-   })
-})
+   // Paginação
+   pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true,
+   },
 
-const elements = document.querySelectorAll('.hidden')
+   // Botões de navegação
+   navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+   },
 
-elements.forEach( (Element) =>my0bserver.observe (Element))
-
+   // Breakpoints corretos
+   breakpoints: {
+      0: {
+         slidesPerView: 1, // Tela pequena (mobile)
+      },
+      620: {
+         slidesPerView: 2, // Tela média (tablets)
+      },
+      1024: {
+         slidesPerView: 3, // Tela grande (desktops)
+      },
+   },
+});
